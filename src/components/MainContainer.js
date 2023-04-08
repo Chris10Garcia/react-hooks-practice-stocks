96cecf634330c17448d7d3c5ccb993074942841b
Christian Garcia <bones192nd@gmail.com>
2023-04-08 14:59:10 -0400

    static features working

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,18 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import StockContainer from "./StockContainer";
 import PortfolioContainer from "./PortfolioContainer";
 import SearchBar from "./SearchBar";
+const API = ''
 
 function MainContainer() {
+  const [stocks, setStocks] = useState([])
+  const [purchased, setPurchased] = useState([])
+
+  useEffect( () => {
+
+    fetch('http://localhost:3001/stocks')
+    .then( r => r.json())
+    .then( d => setStocks(d))
+  }, [])
+
   return (
     <div>
       <SearchBar />
       <div className="row">
         <div className="col-8">
-          <StockContainer />
+          <StockContainer stocks={stocks}/>
         </div>
         <div className="col-4">
-          <PortfolioContainer />
+          <PortfolioContainer stocks={purchased} />
         </div>
       </div>
     </div>
--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Stock from "./Stock";
 
-function PortfolioContainer() {
+function PortfolioContainer({stocks}) {
   return (
     <div>
       <h2>My Portfolio</h2>
-      {
-        //render your portfolio stocks here
-      }
+      { stocks.length > 0 
+          ? stocks.map( stock => { return <Stock stock={stock} key={stock.id}/> } ) 
+          : null }
     </div>
   );
 }
